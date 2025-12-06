21559487c74c968f77c01f3dbd018ec3dd645bf3
Toastedngl <162914629+Toastedngl@users.noreply.github.com>
2025-12-06 01:41:06 -0600

    Create code.py

---
+++ code.py
@@ -0,0 +1,34 @@
+import random
+
+options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(options)
+
+player_choice = input("rock, paper, or scissors?").lower()
+
+if player_choice = ("rock") and computer_choice = ("paper")
+
+  print (f"Computer wins!")
+
+elif player_choice = ("rock") and computer_choice = ("scissors")
+
+  print (f"Player wins!")
+
+elif player_choice = ("scissors") and computer_choice = ("paper")
+
+  print (f"Player wins!")
+
+elif player_choice = ("scissors") and computer_choice = ("rock")
+
+  print (f"Computer wins!")
+
+elif player_choice = ("paper") and computer_choice = ("rock")
+
+  print (f"Player wins!")
+
+elif player_choice = ("paper") and computer_choice = ("scissors")
+
+  print (f"Computer wins!")
+
+else:
+
+  print (f"It's a tie!")
