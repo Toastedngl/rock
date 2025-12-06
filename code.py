ceb4459402a86e71b908a59f0cfda4b634d54e80
Toastedngl <162914629+Toastedngl@users.noreply.github.com>
2025-12-06 01:50:47 -0600

    Update code.py

--- code.py
+++ code.py
@@ -1,31 +1,34 @@
 import random
 
-options = ["rock", "paper", "scissors"]
+options = ["Rock", "Paper", "Scissors"]
 computer_choice = random.choice(options)
+
 
 player_choice = input("rock, paper, or scissors?").lower()
 
-if player_choice = ("rock") and computer_choice = ("paper")
+print (f"The computer chose : {computer_choice}!")
+
+if player_choice == ("rock") and computer_choice == ("paper"):
 
   print (f"Computer wins!")
 
-elif player_choice = ("rock") and computer_choice = ("scissors")
+elif player_choice == ("rock") and computer_choice == ("scissors"):
 
   print (f"Player wins!")
 
-elif player_choice = ("scissors") and computer_choice = ("paper")
+elif player_choice == ("scissors") and computer_choice == ("paper"):
 
   print (f"Player wins!")
 
-elif player_choice = ("scissors") and computer_choice = ("rock")
+elif player_choice == ("scissors") and computer_choice == ("rock"):
 
   print (f"Computer wins!")
 
-elif player_choice = ("paper") and computer_choice = ("rock")
+elif player_choice == ("paper") and computer_choice == ("rock"):
 
   print (f"Player wins!")
 
-elif player_choice = ("paper") and computer_choice = ("scissors")
+elif player_choice == ("paper") and computer_choice == ("scissors"):
 
   print (f"Computer wins!")
 
